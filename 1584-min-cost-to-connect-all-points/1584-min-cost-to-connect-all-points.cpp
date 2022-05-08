e4e0313782efa4e999db35cb7c1e33c61f252276
Mohamed Foula <52828468+Mo-Foula@users.noreply.github.com>
2022-05-08 14:26:39 +0200

    Time: 2099 ms (5.03%), Space: 246.5 MB (6.58%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,98 @@
+class Solution {
+public:
+    class DSU {
+    int* parent;
+    int* rank;
+
+public:
+    DSU(int n)
+    {
+        parent = new int[n];
+        rank = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = -1;
+            rank[i] = 1;
+        }
+    }
+
+    // Find function
+    int find(int i)
+    {
+        if (parent[i] == -1)
+            return i;
+
+        return parent[i] = find(parent[i]);
+    }
+    // union function
+    void unite(int x, int y)
+    {
+        int s1 = find(x);
+        int s2 = find(y);
+
+        if (s1 != s2) {
+            if (rank[s1] < rank[s2]) {
+                parent[s1] = s2;
+                rank[s2] += rank[s1];
+            }
+            else {
+                parent[s2] = s1;
+                rank[s1] += rank[s2];
+            }
+        }
+    }
+};
+
+class Graph {
+    vector<vector<int> > edgelist;
+    int V;
+
+public:
+    Graph(int V) { this->V = V; }
+
+    void addEdge(int x, int y, int w)
+    {
+        edgelist.push_back({ w, x, y });
+    }
+
+    int kruskals_mst()
+    {
+        // 1. Sort all edges
+        sort(edgelist.begin(), edgelist.end());
+
+        // Initialize the DSU
+        DSU s(V);
+        int ans = 0;
+        cout << "Following are the edges in the "
+                "constructed MST"
+             << endl;
+        for (auto edge : edgelist) {
+            int w = edge[0];
+            int x = edge[1];
+            int y = edge[2];
+
+            // take that edge in MST if it does form a cycle
+            if (s.find(x) != s.find(y)) {
+                s.unite(x, y);
+                ans += w;
+                cout << x << " -- " << y << " == " << w
+                     << endl;
+            }
+        }
+        cout << "Minimum Cost Spanning Tree: " << ans;
+        return ans;
+    }
+};
+
+
+int minCostConnectPoints(vector<vector<int>>& points) {
+    Graph g(points.size());
+    for (int i = 0; i <points.size() ; ++i) {
+        for (int j = i+1; j <points.size() ; ++j) {
+            g.addEdge(i,j,abs((points[i][0] - points[j][0])) +abs(( points[i][1] - points[j][1])));
+        }
+    }
+    return g.kruskals_mst();
+}
+
+};
