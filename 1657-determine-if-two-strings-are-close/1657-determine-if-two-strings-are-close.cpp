65147a2cb165a00287759d9aaf5438cac503c9f3
Mohamed Foula <52828468+Mo-Foula@users.noreply.github.com>
2022-12-04 02:29:18 +0200

    Time: 130 ms (80.32%), Space: 24.8 MB (5.59%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -0,0 +1,47 @@
+class Solution
+{
+    public:
+
+        unordered_map<char, int> makeFrequencyMapOfStringChars(string s)
+        {
+            unordered_map<char, int> m;
+            for (auto c: s)
+            {
+                m[c]++;
+            }
+            return m;
+        }
+
+    multiset<int> frequenciesMapToSet(unordered_map<char, int> m)
+    {
+        multiset<int> set;
+        for (auto x: m)
+        {
+            set.insert(x.second);
+            set.insert(-x.first);
+        }
+        return set;
+    }
+
+    bool closeStrings(string word1, string word2)
+    {
+        if (word1.size() != word2.size()) return false;
+
+        unordered_map<char, int> m1 = makeFrequencyMapOfStringChars(word1);
+        unordered_map<char, int> m2 = makeFrequencyMapOfStringChars(word2);
+
+    multiset<int> s1 = frequenciesMapToSet(m1);
+    multiset<int> s2 = frequenciesMapToSet(m2);
+
+        multiset<int>::iterator it1 = s1.begin();
+        multiset<int>::iterator it2 = s2.begin();
+
+        while (it1 != s1.end())
+        {
+            if (*it1 != *it2) return false;
+            it1++;
+            it2++;
+        }
+        return true;
+    }
+};
