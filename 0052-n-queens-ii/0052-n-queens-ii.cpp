c82b3c3152edcf73e0c06ef4c8cb368ee234e3ce
Mohamed Foula <52828468+Mo-Foula@users.noreply.github.com>
2023-03-07 01:22:34 +0200

    Time: 7 ms (46.56%), Space: 10.8 MB (15.94%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,98 @@
+class Solution
+{
+    public:
+
+        bool isAvailable(vector<string> &board, int x, int y)
+        {
+            char c = board[x][y];
+            return c == '.';
+        }
+
+    bool fillForDiagonal(vector<string> &board, int x, int y)
+    {
+        if (x < board.size() && y < board[x].size())
+        {
+            board[x][y] = 'X';
+            return true;
+        }
+        return false;
+    }
+
+    void fillDiagonallyRight(vector<string> &board, int x, int y)
+    {
+        if (fillForDiagonal(board, x, y))
+            fillDiagonallyRight(board, x + 1, y + 1);
+    }
+
+    void fillDiagonallyLeft(vector<string> &board, int x, int y)
+    {
+        if (fillForDiagonal(board, x, y))
+            fillDiagonallyLeft(board, x + 1, y - 1);
+    }
+
+    void fillDiagonally(vector<string> &board, int x, int y)
+    {
+        fillDiagonallyLeft(board, x + 1, y - 1);
+        fillDiagonallyRight(board, x + 1, y + 1);
+    }
+
+    void fillVerticallyAndDiagonally(vector<string> &board, int x, int y)
+    {
+       	// Fill vertically
+        for (int i = x + 1; i < board.size(); i++)
+        {
+            board[i][y] = 'X';
+        }
+
+       	// Fill diagonally
+        fillDiagonally(board, x, y);
+    }
+
+    void cleanBoard(vector<string> &board)
+    {
+        int n = board.size();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (board[i][j] == 'X')
+                    board[i][j] = '.';
+            }
+        }
+    }
+
+    int soln = 0;
+
+    void solve(vector<string> &board, int x, int y)
+    {
+        while (y < board[x].size())
+        {
+
+            if (isAvailable(board, x, y))
+            {
+                vector<string> newBoard(board);
+                newBoard[x][y] = 'Q';
+                fillVerticallyAndDiagonally(newBoard, x, y);
+                if (x + 1 < newBoard.size())
+                {
+                    solve(newBoard, x + 1, 0);
+                }
+                else
+                {
+                    cleanBoard(newBoard);
+                    soln++;
+                }
+            }
+            ++y;
+        }
+    }
+
+    int totalNQueens(int n)
+    {
+        string empty(n, '.');
+        vector<string> board = vector<string> (n, empty);
+        solve(board, 0, 0);
+
+        return soln;
+    }
+};
