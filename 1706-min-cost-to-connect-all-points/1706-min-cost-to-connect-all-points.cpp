43540e9bfbff87cdfeddecceafa6482619ececcc
Mohamed Foula <52828468+Mo-Foula@users.noreply.github.com>
2023-09-15 17:42:52 +0300

    Time: 519 ms (57.44%), Space: 32.9 MB (88.39%) - LeetHub

---
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+int minCostConnectPoints(vector<vector<int>> &points) {
+    
+    vector<vector<int> > myGraph(points.size(), vector<int> (points.size()));
+    int val;
+    for (int i = 0; i < points.size(); ++i) {
+        for (int j = i + 1; j < points.size(); ++j) {
+            myGraph[j][i] = myGraph[i][j] = abs((points[i][0] - points[j][0])) + abs((points[i][1] - points[j][1]));
+        }
+    }
+    
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<> > pq;
+    map<int, bool> visited;
+    pq.push({0, 0});
+    map<int, int> scores,parent;
+    scores[0] = 0;
+    int child;
+    while (!pq.empty()) {
+        auto element = pq.top();
+        pq.pop();
+        if(visited[element.second])
+            continue;
+        visited[element.second] = true;
+        for (int i = 0; i < myGraph[element.second].size(); i++) {
+            if (!visited[i]) {
+                parent[i] = element.second;
+                if (!scores[i]) {
+                    scores[i] = myGraph[element.second][i];
+                    pq.push({myGraph[element.second][i],i});
+                } else{
+                    if(scores[i] > myGraph[element.second][i]){
+                    scores[i] = myGraph[element.second][i];
+                    pq.push({myGraph[element.second][i],i});
+                    }
+                }
+
+            }
+        }
+
+    }
+    int sum = 0;
+    for (int k = 1; k <myGraph.size() ; ++k) {
+        sum += scores[k];
+    }
+    return sum;
+    //     score node
+}
+
+};
